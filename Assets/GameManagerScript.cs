d7da0ca79458e9d77e5b54ff1862ad92bcfeee83
WadaKeita <k023g0069@g.neec.ac.jp>
2024-05-14 15:43:05 +0900

    第四回_滑らかな移動を完了、壁の実装を完了

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -1,6 +1,7 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
@@ -9,6 +10,7 @@ public class GameManagerScript : MonoBehaviour
     public GameObject boxPrefab;
     public GameObject goalPrefab;
     public GameObject particlePrefab;
+    public GameObject wallPrefab;
 
     public GameObject clearText;
 
@@ -37,6 +39,8 @@ public class GameManagerScript : MonoBehaviour
         // 縦軸横軸の配列外参照をしていないか
         if (moveTo.y < 0 || moveTo.y >= field.GetLength(0)) { return false; }
         if (moveTo.x < 0 || moveTo.x >= field.GetLength(1)) { return false; }
+        // 壁が無いか
+        if(field[moveTo.y, moveTo.x] != null && field[moveTo.y, moveTo.x].tag == "Wall") { return false; }
 
         // Boxタグを持っていたら再帰処理
         if (field[moveTo.y, moveTo.x] != null && field[moveTo.y, moveTo.x].tag == "Box")
@@ -50,8 +54,13 @@ public class GameManagerScript : MonoBehaviour
             if (!success) { return false; }
         }
         // GameObjectの座標(position)を移動させてからインデックスの入れ替え
-        field[moveFrom.y, moveFrom.x].transform.position =
-            new Vector3(moveTo.x - field.GetLength(1) / 2, -moveTo.y + field.GetLength(0) / 2, 0);
+        //field[moveFrom.y, moveFrom.x].transform.position =
+        //    new Vector3(moveTo.x - field.GetLength(1) / 2, -moveTo.y + field.GetLength(0) / 2, 0);
+        Vector3 moveToPosition = new Vector3(
+            moveTo.x - map.GetLength(1) / 2, -moveTo.y + map.GetLength(0) / 2, 0
+        );
+        field[moveFrom.y, moveFrom.x].GetComponent<Move>().MoveTo(moveToPosition);
+
         field[moveTo.y, moveTo.x] = field[moveFrom.y, moveFrom.x];
         field[moveFrom.y, moveFrom.x] = null;
 
@@ -72,12 +81,12 @@ public class GameManagerScript : MonoBehaviour
         // Vector2Int型の可変長配列の作成
         List<Vector2Int> goals = new List<Vector2Int>();
 
-        for(int y = 0;y<map.GetLength(0);y++)
+        for (int y = 0; y < map.GetLength(0); y++)
         {
-            for(int x=0;x<map.GetLength(1);x++)
+            for (int x = 0; x < map.GetLength(1); x++)
             {
                 // 格納場所か否かを判断
-                if (map[y,x] == 3)
+                if (map[y, x] == 3)
                 {
                     // 格納場所のインデックスを控えておく
                     goals.Add(new Vector2Int(x, y));
@@ -85,7 +94,7 @@ public class GameManagerScript : MonoBehaviour
             }
         }
         // 要素数はgoals.Countで取得
-        for(int i = 0;i<goals.Count;i++)
+        for (int i = 0; i < goals.Count; i++)
         {
             GameObject f = field[goals[i].y, goals[i].x];
             if (f == null || f.tag != "Box")
@@ -98,6 +107,11 @@ public class GameManagerScript : MonoBehaviour
         return true;
     }
 
+    public void StageReset()
+    {
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -105,13 +119,15 @@ public class GameManagerScript : MonoBehaviour
 
         // 配列の実態の作成と初期化
         map = new int[,] {
-            { 0, 0, 0, 0, 0, 0, 0},
-            { 0, 3, 3, 0, 0, 0, 0},
-            { 0, 2, 2, 0, 0, 0, 0},
-            { 0, 0, 0, 1, 0, 2, 0},
-            { 0, 0, 0, 0, 0, 2, 0},
-            { 0, 0, 0, 0, 0, 3, 0},
-            { 0, 0, 0, 0, 0, 0, 0}
+            { 4, 4, 4, 4, 4, 4, 4, 4, 4},
+            { 4, 0, 0, 0, 0, 0, 0, 0, 4},
+            { 4, 0, 3, 3, 0, 0, 0, 0, 4},
+            { 4, 0, 2, 2, 0, 0, 0, 0, 4},
+            { 4, 0, 0, 0, 1, 0, 2, 0, 4},
+            { 4, 0, 0, 0, 0, 0, 2, 0, 4},
+            { 4, 0, 0, 0, 0, 0, 3, 0, 4},
+            { 4, 0, 0, 0, 0, 0, 0, 0, 4},
+            { 4, 4, 4, 4, 4, 4, 4, 4, 4}
         };
         field = new GameObject
         [
@@ -147,6 +163,14 @@ public class GameManagerScript : MonoBehaviour
                         Quaternion.identity
                     );
                 }
+                if (map[y, x] == 4)
+                {
+                    field[y, x] = Instantiate(
+                        wallPrefab,
+                        new Vector3(x - map.GetLength(1) / 2, -y + map.GetLength(0) / 2, 0),
+                        Quaternion.identity
+                    );
+                }
             }
         }
     }
@@ -154,6 +178,12 @@ public class GameManagerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // ステージのリセット
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StageReset();
+        }
+
         // 右移動
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
